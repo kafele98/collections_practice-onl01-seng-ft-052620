db2d5b22b1c10c03b88ada21e8d4b1c81c41b0c3
Zoe Keller <keller.zoe98@gmail.com>
2020-06-09 13:55:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,7 +3,9 @@ def sort_array_asc (array)
 end
 
 def sort_array_desc (array)
-  array.sort! {|a,b| a <=> b}
+  array.sort do |left, right|
+    right <=> left
+  end 
 end 
 
 # def sort_array_char_count (array)
